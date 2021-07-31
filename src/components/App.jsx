e2db71dfed99e76c04ea37d2e18ee04a2fc9f03e
Daniel Portillo <depr.daniel@gmail.com>
2021-07-31 14:10:47 +0200

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import '../components/style.css';
+
+function App() {
+  return (
+    <div>
+      <section className="App">
+        
+      </section>
+      <footer>
+      <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://dportillo23.github.io/My-Personal-Site/">Daniel Portillo</a>.
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
